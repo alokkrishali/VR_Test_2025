6dd03a8b5e31732c477f49b84c89a145fa5c2e78
Alok Krishali <iamalokkrishali28@gmail.com>
2025-03-20 19:41:55 +0530

    create first VR app
    sucessfuly created first VR app

---
+++ MY_VR_2025/Assets/OnClick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OnClick : ObjectController
+{
+    [SerializeField] TextMeshProUGUI blackSpiderText, sandSpiderText;
+
+   public new void OnPointerClick()
+    {
+        //if(blackSpiderText != null)
+            //blackSpiderText.text = "Black Spider";
+        //if(blackSpiderText != null)
+            //sandSpiderText.text = "Sand Spider";
+    }
+    void Update()
+    {
+        
+    }
+}
